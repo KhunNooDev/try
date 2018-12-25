d33be7aeed26a62e62d10d42a2d43050594f1358
khunnoodev <noo_okok@hotmail.com>
2018-12-25 19:31:58 +0700

    Updates

---
+++ precache-manifest.b33b572e3818e0e26fbc77b440eb42a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "401641913b39f98a530ccdbfe25576ff",
+    "url": "/try/static/media/logo3.40164191.png"
+  },
+  {
+    "revision": "ec58905de7f279262add",
+    "url": "/try/static/js/main.ec58905d.chunk.js"
+  },
+  {
+    "revision": "8e69111e6ac387b80a7423396c0f487d",
+    "url": "/try/static/media/3.8e69111e.jpg"
+  },
+  {
+    "revision": "9f0f43cc119c9e1f8949",
+    "url": "/try/static/js/runtime~main.9f0f43cc.js"
+  },
+  {
+    "revision": "45258559d833ab8720ef7b2b64b791fb",
+    "url": "/try/static/media/logo1.45258559.png"
+  },
+  {
+    "revision": "40b1669a89e360029e7d11f693e560af",
+    "url": "/try/static/media/logo2.40b1669a.png"
+  },
+  {
+    "revision": "dda60bc779fe0d76acac91fd4f4561ca",
+    "url": "/try/static/media/2.dda60bc7.jpg"
+  },
+  {
+    "revision": "6a1f24f7d8813769b142c015909092b9",
+    "url": "/try/static/media/TML.6a1f24f7.png"
+  },
+  {
+    "revision": "6bc27d76be682e18b6cd1f038043f65e",
+    "url": "/try/static/media/1.6bc27d76.jpg"
+  },
+  {
+    "revision": "3680e2f41fd3c7c8faee",
+    "url": "/try/static/js/1.3680e2f4.chunk.js"
+  },
+  {
+    "revision": "3680e2f41fd3c7c8faee",
+    "url": "/try/static/css/1.f04b3d66.chunk.css"
+  },
+  {
+    "revision": "9910409c30f21d06c08bbd55147ba58e",
+    "url": "/try/index.html"
+  }
+];
